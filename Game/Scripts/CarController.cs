0fc19bdfae36ed6ed3ae2c770f0f0b8b3a5adf6e
MuhammadUsama021 <muhammadusama008233@gmail.com>
2021-03-20 21:55:34 +0500

    Add CarController Script

---
+++ Game/Scripts/CarController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float fuel = 1;
+    public float fuelcomsumption = 0.1f;
+    public Rigidbody2D carRigidbody;
+    public Rigidbody2D backTire;
+    public Rigidbody2D frontTire;
+    public float carTorque = 10;
+    public float speed = 20;
+    private float movement;
+    public UnityEngine.UI.Image image;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        image.fillAmount = fuel;
+
+        
+    }
+    private void FixedUpdate()
+    {
+        if (fuel > 0)
+        {
+            backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            carRigidbody.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
+        }
+        fuel -= fuelcomsumption * Mathf.Abs(movement) * Time.fixedDeltaTime;
+        
+    }
+    
+    
+}
