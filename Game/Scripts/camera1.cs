a7ac6472cc8c428e8d623c1f36222bfa07a2b0ef
muhammadnadeem074 <fa18-bse-074@cuiatk.edu.pk>
2021-03-21 10:02:56 +0500

    Add Camera and Fuelfill scripts

---
+++ Game/Scripts/camera1.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class camera1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    private Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - target.position;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset;
+        
+    }
+}
